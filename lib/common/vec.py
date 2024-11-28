3b3360fc7493eb9ca58e9f0028a562fa0cdf3b71
Sean Malloy <techfan321@gmail.com>
2024-11-28 12:12:16 -0500

    cleanup vec library

--- lib/common/vec.py
+++ lib/common/vec.py
@@ -1,30 +1,6 @@
 from dataclasses import dataclass
-from typing import Self
+from typing import Self, Any
 from math import sqrt
-
-
-@dataclass(frozen=True, eq=True, order=True)
-class Point:
-    r: int
-    c: int
-
-    def __add__(self, other: Self):
-        return Point(self.r + other.r, self.c + other.c)
-    
-    def __sub__(self, other: Self):
-        return Point(self.r - other.r, self.c - other.c)
-        
-    def in_bounds(self, grid: list[list]):
-        return self.r >= 0 and self.c >= 0 and self.r < len(grid) and self.c < len(grid[self.r])
-
-    def dist(self, other: Self) -> float:
-        return sqrt((self.r - other.r)**2 + (self.c - other.c)**2)
-
-    def mdist(self, other: Self):
-        return abs(self.r - other.r) + abs(self.c - other.c)
-    
-    def __repr__(self) -> str:
-        return f'({self.r}, {self.c})'
 
 
 @dataclass(frozen=True, eq=True, order=True)
@@ -32,34 +8,62 @@ class Vec2:
     x: int
     y: int
 
+    @property
+    def r(self):
+        return self.x
+
+    @property
+    def c(self):
+        return self.y
+    
+    @r.setter
+    def r(self, v):
+        self.x = v
+    
+    @c.setter
+    def c(self, v):
+        self.y = v
+
     def __add__(self, other: Self):
         return Vec2(self.x + other.x, self.y + other.y)
     
     def __sub__(self, other: Self):
         return Vec2(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other):
+    def __mul__(self, other: int):
         return Vec2(self.x * other, self.y * other)
     
     def __rmul__(self, other: int):
         return self.__mul__(other)
     
-    def __truediv__(self, other):
+    def __truediv__(self, other: int | float):
+        if not (isinstance(other, int) or isinstance(other, float)):
+            return NotImplemented
         return Vec2(self.x / other, self.y / other)
 
-    def __floordiv__(self, other: int):
+    def __floordiv__(self, other: int | float):
+        if not (isinstance(other, int) or isinstance(other, float)):
+            return NotImplemented
         return Vec2(self.x // other, self.y // other)
 
     def dot(self, other: Self):
+        if not isinstance(other, Vec2):
+            return NotImplemented
         return self.x * other.x + self.y * other.y
     
     def cross(self, other: Self):
+        if not isinstance(other, Vec2):
+            return NotImplemented
         return self.x*other.y - self.y*other.x
     
-    def dist(self, other: Self) -> float:
+    def distance(self, other: Self) -> float:
+        if not isinstance(other, Vec2):
+            return NotImplemented
         return sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
 
-    def mdist(self, other: Self):
+    def manhattan_distance(self, other: Self):
+        if not isinstance(other, Vec2):
+            return NotImplemented
         return abs(self.x - other.x) + abs(self.y - other.y)
 
     def magnitude(self):
@@ -68,11 +72,14 @@ class Vec2:
     def normalized(self):
         return self / self.magnitude()
 
+    def in_bounds(self, grid: list[list[Any]]):
+        return self.r >= 0 and self.c >= 0 and self.r < len(grid) and self.c < len(grid[self.r])
+
     def __repr__(self) -> str:
         return f'({self.x}, {self.y})'
 
 
-class Vec2Dir:
+class VecDir:
     N = Vec2(0, 1)
     S = Vec2(0, -1)
     E = Vec2(1, 0)
@@ -94,8 +101,37 @@ class Vec2Dir:
 
     @staticmethod
     def opposite(d: Self):
-        return Vec2._opposite[d]
-    
+        return VecDir._opposite[d]
+
     @staticmethod
     def all():
-        return Vec2._all
+        return VecDir._all
+
+
+class PointDir:
+    N = Vec2(-1, 0)
+    S = Vec2(1, 0)
+    E = Vec2(0, 1)
+    W = Vec2(0, -1)
+
+    L = W
+    R = E
+    U = N
+    D = S
+
+    _opposite = {
+        N: S,
+        S: N,
+        E: W,
+        W: E
+    }
+
+    _all = {N, S, E, W}
+
+    @staticmethod
+    def opposite(d: Self):
+        return PointDir._opposite[d]
+
+    @staticmethod
+    def all():
+        return PointDir._all
